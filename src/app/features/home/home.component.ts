13f59949fca10ef08a5517e79bf47157b0d5e5e9
Lars <lars@k7c.dk>
2024-07-23 17:10:25 +0200

    tilføjet video

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,15 +1,24 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
 import { PageComponent } from '../../shared/components/core/page/page.component';
-import { NgOptimizedImage } from '@angular/common';
+import Player from '@vimeo/player';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [PageComponent, NgOptimizedImage],
+  imports: [PageComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('playerContainer') playerContainer: ElementRef;
+  url = 'https://vimeo.com/311435084';
 
+  constructor() {
+    this.playerContainer = new ElementRef(null);
+  }
+
+  ngAfterViewInit() {
+    new Player(this.playerContainer.nativeElement, { url: this.url, width: 300 });
+  }
 }
