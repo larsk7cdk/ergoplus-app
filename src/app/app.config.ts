7944cd2da43a45ef45f83d7738fd707a6e249b38
Lars <lars@k7c.dk>
2024-07-24 15:22:55 +0200

    tilføjet cookie consent

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,54 @@
 import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideRouter, withInMemoryScrolling, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 import localeDa from '@angular/common/locales/da';
 import { HashLocationStrategy, LocationStrategy, registerLocaleData } from '@angular/common';
+import { NgcCookieConsentConfig, provideNgcCookieConsent } from 'ngx-cookieconsent';
+import { environment } from '../environments/environment';
 
 registerLocaleData(localeDa);
+
+const cookieConfig: NgcCookieConsentConfig = {
+  'cookie': {
+    'domain': environment.cookieDomain
+  },
+  'position': 'bottom',
+  'theme': 'classic',
+  'palette': {
+    'popup': {
+      'background': '#72a88d',
+      'text': '#ffffff',
+      'link': '#ffffff'
+    },
+    'button': {
+      'background': '#f45b07',
+      'text': '#000000',
+      'border': 'transparent'
+    }
+  },
+  'type': 'info',
+  'content': {
+    'message': 'Denne hjemmeside bruger cookies for at sikre, at du får den bedste oplevelse på hjemmesiden.',
+    'dismiss': 'Tillad cookies',
+    'deny': 'Afvis cookies',
+    'link': '',
+    'href': 'https://cookiesandyou.com',
+    'policy': 'Cookie Policy'
+  }
+};
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withViewTransitions()),
+    provideRouter(routes,
+      withViewTransitions(),
+      withInMemoryScrolling({ scrollPositionRestoration: 'top' })
+    ),
+    provideNgcCookieConsent(cookieConfig),
     provideClientHydration(),
     { provide: LOCALE_ID, useValue: 'da-DK' },
     { provide: LocationStrategy, useClass: HashLocationStrategy }]
