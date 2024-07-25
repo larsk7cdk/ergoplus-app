79d876d12b832f06e550ffac8c0c47d99a60360a
JB6504 <lal@jyskebank.dk>
2024-07-25 10:01:30 +0200

    skiftende farver på priser

--- src/app/features/prices/prices.component.ts
+++ src/app/features/prices/prices.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { PageComponent } from '../../shared/components/core/page/page.component';
-import { NgTemplateOutlet } from '@angular/common';
+import { NgClass, NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-prices',
   standalone: true,
-  imports: [PageComponent, NgTemplateOutlet],
+  imports: [PageComponent, NgTemplateOutlet, NgClass],
   templateUrl: './prices.component.html',
   styleUrl: './prices.component.scss'
 })
