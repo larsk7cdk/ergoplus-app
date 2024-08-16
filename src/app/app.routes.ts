0b9b62f191578d6595197b716492001f779be09b
Lars Larsen <larsk7cdk@users.noreply.github.com>
2024-08-16 14:17:44 +0200

    fixed navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { HomeComponent } from './features/home/home.component';
 export const routes: Routes = [
   {
     path: 'home',
+    title: 'Forside',
     component: HomeComponent
   },
   {
     path: 'prices',
+    title: 'Priser',
     loadComponent: () => import('./features/prices/prices.component')
       .then(m => m.PricesComponent)
   },
