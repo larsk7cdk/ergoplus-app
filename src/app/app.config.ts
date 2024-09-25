14d30f5b1406e31a345573e8315271a14b988292
Lars Larsen <lal@jyskebank.dk>
2024-09-25 15:05:08 +0200

    fixed url og location strategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,52 +1,69 @@
-import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withInMemoryScrolling, withViewTransitions } from '@angular/router';
+import {
+  ApplicationConfig,
+  LOCALE_ID,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import {
+  provideRouter,
+  withInMemoryScrolling,
+  withViewTransitions,
+} from '@angular/router';
 import { routes } from './app.routes';
 
 import localeDa from '@angular/common/locales/da';
-import { HashLocationStrategy, LocationStrategy, registerLocaleData } from '@angular/common';
-import { NgcCookieConsentConfig, provideNgcCookieConsent } from 'ngx-cookieconsent';
+import {
+  LocationStrategy,
+  PathLocationStrategy,
+  registerLocaleData,
+} from '@angular/common';
+import {
+  NgcCookieConsentConfig,
+  provideNgcCookieConsent,
+} from 'ngx-cookieconsent';
 import { environment } from '../environments/environment';
 
 registerLocaleData(localeDa);
 
 const cookieConfig: NgcCookieConsentConfig = {
-  'cookie': {
-    'domain': environment.cookieDomain
+  cookie: {
+    domain: environment.cookieDomain,
   },
-  'position': 'bottom',
-  'theme': 'classic',
-  'palette': {
-    'popup': {
-      'background': '#72a88d',
-      'text': '#ffffff',
-      'link': '#ffffff'
+  position: 'bottom',
+  theme: 'classic',
+  palette: {
+    popup: {
+      background: '#72a88d',
+      text: '#ffffff',
+      link: '#ffffff',
     },
-    'button': {
-      'background': '#f45b07',
-      'text': '#000000',
-      'border': 'transparent'
-    }
+    button: {
+      background: '#f45b07',
+      text: '#000000',
+      border: 'transparent',
+    },
   },
-  'type': 'info',
-  'content': {
-    'message': 'Denne hjemmeside bruger cookies for at sikre, at du får den bedste oplevelse på hjemmesiden.',
-    'dismiss': 'Tillad cookies',
-    'deny': 'Afvis cookies',
-    'link': '',
-    'href': 'https://cookiesandyou.com',
-    'policy': 'Cookie Policy'
-  }
+  type: 'info',
+  content: {
+    message:
+      'Denne hjemmeside bruger cookies for at sikre, at du får den bedste oplevelse på hjemmesiden.',
+    dismiss: 'Tillad cookies',
+    deny: 'Afvis cookies',
+    link: '',
+    href: 'https://cookiesandyou.com',
+    policy: 'Cookie Policy',
+  },
 };
-
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes,
+    provideRouter(
+      routes,
       withViewTransitions(),
-      withInMemoryScrolling({ scrollPositionRestoration: 'top' })
+      withInMemoryScrolling({ scrollPositionRestoration: 'top' }),
     ),
     provideNgcCookieConsent(cookieConfig),
     { provide: LOCALE_ID, useValue: 'da-DK' },
-    { provide: LocationStrategy, useClass: HashLocationStrategy }]
+    { provide: LocationStrategy, useClass: PathLocationStrategy },
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,31 +3,31 @@ import { HomeComponent } from './features/home/home.component';
 
 export const routes: Routes = [
   {
-    path: 'home',
+    path: 'forside',
     title: 'Forside',
     data: { title: 'Ergoterapi \nGør din hverdag mulig' },
     component: HomeComponent
   },
   {
-    path: 'ergotherapy',
+    path: 'ergoterapi',
     title: 'Ergoterapi',
     data: { title: 'Ergoterapi' },
     loadComponent: () => import('./features/ergotherapy/ergotherapy.component')
       .then(m => m.ErgotherapyComponent)
   },
   {
-    path: 'prices',
+    path: 'priser',
     title: 'Priser',
     data: { title: 'Priser' },
     loadComponent: () => import('./features/prices/prices.component')
       .then(m => m.PricesComponent)
   },
   {
-    path: 'ergoplus',
+    path: 'omergoplus',
     title: 'Om ErgoPlus',
     data: { title: 'Om ErgoPlus' },
     loadComponent: () => import('./features/ergoplus/ergoplus.component')
       .then(m => m.ErgoplusComponent)
   },
-  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+  { path: '**', redirectTo: 'forside', pathMatch: 'full' }
 ];
